13e28b65f66c689c203f54ee2f3bad13bf33a374
Glitch (glitch-blank-website) <none>
2023-05-18 13:56:58 +0000

    🐾👁 Checkpoint ./public/assets/js/client.js:45080849/998 ./public/assets/js/app.js:45080849/428 ./public/index.html:45080849/2447

---
+++ public/assets/js/app.js
@@ -0,0 +1,15 @@
+import { createClient } from './client.js';
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const socket = io();
+
+function resizeCanvas() {
+  ctx.canvas.width  = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
+}
+
+window.addEventListener("DOMContentLoaded", resizeCanvas);
+window.addEventListener("resize", resizeCanvas);
+
+createClient();
--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -1,11 +1,5 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-const socket = io();
-
-function resizeCanvas() {
-  ctx.canvas.width  = window.innerWidth;
-  ctx.canvas.height = window.innerHeight;
+function createClient() {
+  alert("test");
 }
 
-window.addEventListener("DOMContentLoaded", resizeCanvas);
-window.addEventListener("resize", resizeCanvas);
+export default createClient;
