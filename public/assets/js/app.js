ec9c63a4c4dfde1b5c47759a1880695e9cbaf490
Glitch (glitch-blank-website) <none>
2023-05-22 21:53:33 +0000

    🎯🍈 Checkpoint ./public/assets/js/app.js:45080849/1994 ./public/index.html:45080849/57

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -5,6 +5,14 @@ const client = new Client(socket);
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+function parseForm(formData) {
+  let object = {};
+  
+  formData.forEach((value, key) => object[key] = value);
+  
+  let json = JSON.stringify(object);
+}
+
 function resizeCanvas() {
   ctx.canvas.width  = window.innerWidth;
   ctx.canvas.height = window.innerHeight;
@@ -16,3 +24,18 @@ function resizeCanvas() {
 window.addEventListener("DOMContentLoaded", resizeCanvas);
 window.addEventListener("resize", resizeCanvas);
 
+// Populates and Handles Form Data
+
+$("#new-game").on("submit", function (e) {
+  e.preventDefault();
+  
+  const formData = new FormData(e.target);
+  
+  console.log(formData)
+  
+  // client.createInstance();
+});
+
+$(document).ready(function() {
+  
+});
