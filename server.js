f6b6f4a19611c1bc1ab3a12d36868225d7ef452e
Glitch (glitch-blank-website) <none>
2023-05-22 14:18:38 +0000

    🏥🔮 Checkpoint ./server.js:45080849/2452

--- server.js
+++ server.js
@@ -30,19 +30,43 @@ class Server {
       
     }
   
-    parseRoom({ roomId }) {
+    parseSocket({ socket, socketId }) {
+        if (socket instanceof Socket) {
+            return socket;
+        }
+    }
+
+    parseRoom({ socket, socketId, room, roomId }) {
+        if (typeof socket === 'object' && typeof this.socket[socket.id].roomId === 'string' && this.room[this.socket[socket.id].roomId] instanceof Room) {
+            return this.room[this.socket[socket.id].roomId];
+        }
+
+        if (room instanceof Room) {
+            return room;
+        }
+
+        if (this.room[roomId] instanceof Room) {
+            return this.room[roomId];
+        }
+    }
+  
+    getRandomRoom() {
       
     }
   
-    parseSocket({ socket, socketId }) {
-      
-    }
-
     captureEvent({ event, params }) {
       const navigator = event.split("/");
       
       if (navigator[0] === 'Room') {
+        let room;
+        
         if (navigator[1] === 'Join') {
+          console.log("Checking if roomId was given")
+          
+          let room = this.getRandomRoom();
+          
+          
+          
           console.log("Searching for Rooms...")
           
           console.log(params.socket.id);
@@ -58,21 +82,28 @@ class Server {
   
     createInstance(socket) {
       // Creates On Events for Socket
-      socket.on("Room/Create", (state) => {
+      socket.on("Room/Create", () => {
           this.captureEvent({
             event: 'Room/Create',
             params: { socket }
           });
       });
       
-      socket.on("Room/Join", (state) => {
+      socket.on("Room/Join", (roomId) => {
           this.captureEvent({
-            event: 'Room/Join',
+            event: "Room/Join",
+            params: { roomId, socket }
+          });
+      });
+      
+      socket.on("Room/Kick", (socketId) => {
+          this.captureEvent({
+            event: 'Room/Leave',
             params: { socket }
           });
       });
       
-      socket.on("Room/Leave", (state) => {
+      socket.on("Room/Leave", () => {
           this.captureEvent({
             event: 'Room/Leave',
             params: { socket }
