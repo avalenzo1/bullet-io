ab2a4f19c9b598fedd3ec42d35d305b332d9a78d
Glitch (glitch-blank-website) <none>
2023-05-22 15:12:37 +0000

    🚔🎁 Checkpoint ./blobius.js:45080849/436 ./server.js:45080849/266

--- blobius.js
+++ blobius.js
@@ -1,8 +1,20 @@
 const { uniqueNamesGenerator, adjectives, colors, animals } = require('unique-names-generator');
 
+function UUID() {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+}
+
+function UID() {
+    return Math.random().toString(36).slice(-6).toUpperCase();
+}
+
 class Room {
   constructor(io) {
     this.name = name;
+    this.id = UUID();
+    this.io = io;
     this.socket = {};
     this.capacity = 100;
   }
