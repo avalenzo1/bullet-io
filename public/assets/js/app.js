abfe84617759c89639019668a1c1dbf3f8d2c416
Glitch (glitch-blank-website) <none>
2023-05-23 14:28:12 +0000

    🛢🌼 Checkpoint ./public/index.html:45080849/18 ./public/assets/css/styles.css:45080849/93 ./public/assets/js/blobius.js:45080849/3312 ./public/assets/js/app.js:45080849/2704

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,5 @@
-import { Client } from './blobius.js';
-import { formToJSON } from './tool-box.js'
+import { Client } from "./blobius.js";
+import { formToJSON } from "./tool-box.js";
 
 // Creates Socket Connection
 const socket = io();
@@ -9,7 +9,7 @@ const client = new Client(socket);
 const canvas = document.getElementById("canvas");
 
 function resizeCanvas() {
-  canvas.width  = window.innerWidth;
+  canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 }
 
@@ -20,28 +20,28 @@ window.addEventListener("resize", resizeCanvas);
 
 $("#new-game").on("submit", function (e) {
   e.preventDefault();
-  
+
   // On form submit, convert form input to JSON
   const formData = formToJSON(new FormData(e.target));
-  
+
   // Saves form data to localStorage
   localStorage.setItem("formData", JSON.stringify(formData));
-  
+
   // Starts Client Connection
   client.captureEvent({
     event: "Room/Join",
-    params: { formData }
+    params: { formData },
   });
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
   // If localStorage includes formData, populate data into form
-  
+
   if (localStorage.getItem("formData")) {
     let formData = JSON.parse(localStorage.getItem("formData"));
-    
-    console.log(formData)
-    
+
+    console.log(formData);
+
     // Populates Input Data
     for (let key in formData) {
       if (formData.hasOwnProperty(key)) {
@@ -49,7 +49,7 @@ $(document).ready(function() {
       }
     }
   }
-  
+
   // Creates Socket.IO onEvents
   client.listen();
 });
--- public/assets/js/blobius.js
+++ public/assets/js/blobius.js
@@ -1,27 +1,28 @@
-
 class Game {
-  constructor(canvas) {
+  constructor() {
     this.canvas = document.getElementById("canvas");
-    this.ctx = canvas.getContext("2d");
-    
+    this.ctx = this.canvas.getContext("2d");
+
     // List of all rendered items
     this.playerList = [];
     this.itemList = [];
     this.pelletList = [];
-    
+
     this.showDebug = false;
   }
   
+  tick(room) {
+    
+  }
+
   render() {
     let ctx = this.ctx;
-    
-    ctx.fillText("hi", 12, 12);
-    
+    let canvas = this.canvas;
+
+    ctx.fillStyle = "#ccc"
+    ctx.fillRect(0,0, 100, 100);
+
     window.requestAnimationFrame(this.render);
-  }
-  
-  selfDestruct() {
-    
   }
 }
 
@@ -31,36 +32,35 @@ class Client {
     this.room = null;
     this.game = new Game();
   }
-  
+
   listen() {
     // Creates onEvents for socket
-    
-    this.socket.on('disconnect', function() {
+
+    this.socket.on("disconnect", function () {
       console.log("Socket was disconnected");
     });
-    
-    this.socket.on('connect', function() {
+
+    this.socket.on("connect", function () {
       console.log("Socket was connected");
     });
-    
-    this.socket.on('Room/Join', function(room) {
-      this.room = room;
+
+    this.socket.on("Room/Join", function (room) {
+      alert(JSON.strini)
     });
-                   
-    this.socket.on('Room/Tick', function() {
+    
+    this.socket.on("Room/Tick", function (room) {
       
     });
   }
-  
+
   captureEvent({ event, params }) {
-    if (event === 'Room/Create') {
-      
+    if (event === "Room/Create") {
     }
-    
-    if (event === 'Room/Join') {
+
+    if (event === "Room/Join") {
       this.socket.emit("Room/Join", params.formData);
     }
-    
+
     console.log(`Event "${event}" was captured on ${new Date().toString()}`);
   }
 }
